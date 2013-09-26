64aa59b3bb4425a4cfb809134b69c0dcbb6305b4
Kevin Schaul <kevin.schaul@gmail.com>
2013-09-26 12:01:50 -0500

    Add leaflet precinct map

--- public/base.js
+++ public/base.js
@@ -2,31 +2,58 @@ var map = {
   width: 500,
   height: 500,
 
+  colorScheme: ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02', '#a6761d', '#666666'],
+
   init: function() {
     var self = this;
 
-    self.projection = d3.geo.albers()
-      .scale(60000)
-      .center([2.53, 45.015])
-      .translate([self.width / 2, self.height / 2])
+    self.map = L.map('map-target')
+    self.map.setView([44.985, -93.241], 10);
+    self.addTonerLayer();
+    self.addPrecinctLayer();
+  },
 
-    self.path = d3.geo.path()
-      .projection(self.projection)
+  addTonerLayer: function() {
+    var self = this;
 
-    self.svg = d3.select('#map-target').append('svg')
-      .attr('width', self.width)
-      .attr('height', self.height)
+    self.tonerLayer = new L.TileLayer(
+      'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png',
+      {
+        'opacity': 0.7,
+        'subdomains': ['a', 'b', 'c', 'd'],
+        'attribution': 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.<br />Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+      }
+    );
+
+    self.map.addLayer(self.tonerLayer);
+  },
+
+  addPrecinctLayer: function() {
+    var self = this;
 
     d3.json('precincts-hennepin.json', function(error, data) {
       console.log(data);
+      var geoJson = topojson.feature(data, data.objects.hennepin).features;
+      console.log(geoJson);
 
-      self.svg.append('g')
-        .attr('class', 'precincts')
-        .selectAll('path')
-          .data(topojson.feature(data, data.objects.hennepin).features)
-        .enter().append('path')
-          .attr('d', self.path);
-    })
+      self.precinctLayer = new L.geoJson(geoJson, {
+        'style': function(d) { return self.stylePrecinct(d, self); }
+      });
+
+      self.map.addLayer(self.precinctLayer);
+    });
+  },
+
+  stylePrecinct: function(d, self) {
+    var self = self;
+
+    var rand = Math.floor(Math.random() * 7);
+    return {
+      fillColor: self.colorScheme[rand],
+      fillOpacity: 0.7,
+      weight: 0.5,
+      color: '#fff'
+    };
   }
 }
 
