54c5a74a84dcc83c9fd1389f837eaf51f4f52dff
Kevin Schaul <kevin.schaul@gmail.com>
2013-10-01 12:00:27 -0500

    Fixed input, button styles

--- public/base.js
+++ public/base.js
@@ -1,6 +1,6 @@
 var map = {
   $addressInput: $('input#address'),
-  $addressButton: $('button#address-button'),
+  $addressButton: $('#address-button'),
 
   colorScheme: ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02', '#a6761d', '#666666'],
 
