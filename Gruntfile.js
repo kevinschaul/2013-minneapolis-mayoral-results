4de416e9fc210a4781122ddd644d1331573e840d
Kevin Schaul <kevin.schaul@gmail.com>
2013-10-08 13:41:57 -0500

    Added test command to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,20 @@ module.exports = function(grunt) {
           keepalive: true
         },
       }
+    },
+    mochaTest: {
+      test: {
+        src: ['test/**/*.js']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.registerTask('default', ['connect']);
   grunt.registerTask('server', ['connect']);
+  grunt.registerTask('test', ['mochaTest']);
 
 };
 
