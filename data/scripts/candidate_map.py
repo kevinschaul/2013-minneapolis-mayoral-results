7a5132b1bd3076bf2b0a4e028b55334c8851aba1
Kevin Schaul <kevin.schaul@gmail.com>
2013-10-09 18:44:09 -0500

    Add candidate map - needs to be standardized

---
+++ data/scripts/candidate_map.py
@@ -0,0 +1,164 @@
+candidates = {
+    '9001': {
+        'first_name': 'Alicia',
+        'middle_initial': 'K.',
+        'last_name': 'Bennett',
+    },
+    '9002': {
+        'first_name': 'Mike',
+        'last_name': 'Gould',
+    },
+    '9003': {
+        'first_name': 'Troy',
+        'last_name': 'Benjegerdes',
+    },
+    '9004': {
+        'first_name': 'Ole',
+        'last_name': 'Savior',
+    },
+    '9005': {
+        'first_name': 'Kurtis',
+        'middle_initial': 'W.',
+        'last_name': 'Hanna',
+    },
+    '9006': {
+        'first_name': 'Abdul',
+        'middle_initial': 'M.',
+        'last_name': 'Rahaman',
+        'suffix_nickname': '"The Rock"',
+    },
+    '9007': {
+        'first_name': 'Bill',
+        'last_name': 'Kahn',
+    },
+    '9008': {
+        'first_name': 'Stephanie',
+        'last_name': 'Woodruff',
+    },
+    '9009': {
+        'first_name': 'John',
+        'middle_name': 'Leslie',
+        'last_name': 'Hartwig',
+    },
+    '9010': {
+        'first_name': 'Dan',
+        'last_name': 'Cohen',
+    },
+    '9011': {
+        'first_name': 'Bob',
+        'middle_nickname': '"Again"',
+        'last_name': 'Carney',
+        'suffix_nickname': 'Jr',
+    },
+    '9012': {
+        'first_name': 'James',
+        'last_name': 'Everett',
+    },
+    '9013': {
+        'first_name': 'Don',
+        'last_name': 'Samuels',
+    },
+    '9014': {
+        'first_name': 'Betsy',
+        'last_name': 'Hodges',
+    },
+    '9015': {
+        'first_name': 'Rahn',
+        'middle_initial': 'V.',
+        'last_name': 'Workcuff',
+    },
+    '9016': {
+        'first_name': 'Bob',
+        'last_name': 'Fine',
+    },
+    '9017': {
+        'first_name': 'Mark',
+        'middle_initial': 'V',
+        'last_name': 'Anderson',
+    },
+    '9018': {
+        'first_name': 'Gregg',
+        'middle_initial': 'A.',
+        'last_name': 'Iverson',
+    },
+    '9019': {
+        'first_name': 'Edmund',
+        'middle_name': 'Bernard',
+        'last_name': 'Bruyere',
+    },
+    '9020': {
+        'prefix': 'Captain',
+        'first_name': 'Jack',
+        'last_name': 'Sparrow',
+    },
+    '9021': {
+        'first_name': 'Jackie',
+        'last_name': 'Cherryhomes',
+    },
+    '9022': {
+        'first_name': 'Christopher',
+        'middle_name': 'Robin',
+        'last_name': 'Zimmerman',
+    },
+    '9023': {
+        'first_name': 'Cyd',
+        'last_name': 'Gorman',
+    },
+    '9024': {
+        'first_name': 'Joshua',
+        'last_name': 'Rea',
+    },
+    '9025': {
+        'first_name': 'Merrill',
+        'last_name': 'Anderson',
+    },
+    '9026': {
+        'first_name': 'Doug',
+        'last_name': 'Mann',
+    },
+    '9027': {
+        'first_name': 'James',
+        'middle_nickname': '"Jimmy"',
+        'middle_initial': 'L.',
+        'last_name': 'Stroud',
+        'suffix': 'Jr.',
+    },
+    '9028': {
+        'first_name': 'Tony',
+        'last_name': 'Lane',
+    },
+    '9029': {
+        'first_name': 'Jeffrey',
+        'middle_name': 'Alan',
+        'last_name': 'Wagner',
+    },
+    '9030': {
+        'first_name': 'Christopher',
+        'last_name': 'Clark',
+    },
+    '9031': {
+        'first_name': 'Mark',
+        'last_name': 'Andrew',
+    },
+    '9032': {
+        'first_name': 'Cam',
+        'last_name': 'Winton',
+    },
+    '9033': {
+        'first_name': 'John',
+        'middle_name': 'Charles',
+        'last_name': 'Wilson',
+    },
+    '9034': {
+        'first_name': 'Neal',
+        'last_name': 'Baxter',
+    },
+    '9035': {
+        'first_name': 'Jaymie',
+        'last_name': 'Kelly',
+    },
+    '9901': {
+        'last_name': 'Write-in',
+    },
+}
+
