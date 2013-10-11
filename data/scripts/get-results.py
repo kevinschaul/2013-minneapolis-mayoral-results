d70e10edd3aff5f6615c6c1cf3d9deac3181abe4
Kevin Schaul <kevin.schaul@gmail.com>
2013-10-11 13:01:43 -0500

    Added ftplib exception handling, retry code

--- data/scripts/get-results.py
+++ data/scripts/get-results.py
@@ -1,19 +1,40 @@
 #!/usr/bin/env python
 
-from ftplib import FTP
+import ftplib
+import sys
+import time
+
+ATTEMPTS = 5
 
 USERNAME = 'media'
 PASSWORD = 'results'
 FILENAME = 'localPrct.txt'
 
-ftp = FTP('ftp.sos.state.mn.us')
-ftp.login(USERNAME, PASSWORD)
+attempted = 0
 
-f = open(FILENAME, 'w')
+def getResults():
+    global attempted
+    if attempted < ATTEMPTS:
+        try:
+            attempted += 1
 
-ftp.retrlines('RETR 20131105_MG/localPrct.txt', f.write)
+            ftp = ftplib.FTP('ftp.sos.state.mn.us')
+            ftp.login(USERNAME, PASSWORD)
 
-ftp.quit()
+            with open(FILENAME, 'w') as f:
+                ftp.retrlines('RETR 20131105_MG/localPrct.txt', f.write)
+                ftp.quit()
 
-f.close()
+            print 'File written successfully'
+
+        except ftplib.all_errors, error:
+            sys.stderr.write('ERROR: %s\n' % error)
+            sys.stderr.write('Attempt %d of %d\n' % (attempted, ATTEMPTS))
+            time.sleep(2)
+            getResults()
+    else:
+        print 'Maximum attempts reached.'
+
+if __name__ == '__main__':
+    getResults()
 
