c406b30497d6ea9e444db3533a91a02c5fc821dd
Kevin Schaul <kevin.schaul@gmail.com>
2013-10-15 16:25:07 -0500

    Add output of results.csv for print

--- data/scripts/update-results.py
+++ data/scripts/update-results.py
@@ -82,5 +82,46 @@ with open('../workspace/localPrct.txt', 'r') as f:
                 precinct['total_votes_third'] =  votes_total
                 total['total_votes_third'] += votes
 
-print json.dumps(results, indent=4 * ' ')
+with open('../workspace/results.json', 'w') as f:
+    f.write(json.dumps(results))
+
+# Write file to be joined with shapefile for print
+with open('../workspace/results.csv', 'w') as f:
+    w = csv.writer(f)
+
+    w.writerow([
+        'vtd',
+        'winner_first_name',
+        'winner_last_name',
+        'winner_votes',
+        'total_votes',
+        'winner_percent'
+    ])
+
+    for precinct_id in results['precincts']:
+        precinct = results['precincts'][precinct_id]
+
+        row = []
+
+        winner_id = -1
+        winner_votes = 0
+        for candidate_id in precinct['candidates']:
+            candidate = precinct['candidates'][candidate_id]
+            if int(candidate['first_choice']) >= winner_votes:
+                winner_id = candidate_id
+        winner = precinct['candidates'][winner_id]
+
+        try:
+            winner_percent = winner_votes / precinct['total_votes_first']
+        except ZeroDivisionError:
+            winner_percent = 0
+
+        row.append('27053' + precinct_id)
+        row.append(winner['first_name'])
+        row.append(winner['last_name'])
+        row.append(winner_votes)
+        row.append(precinct['total_votes_first'])
+        row.append(winner_percent)
+
+        w.writerow(row)
 
