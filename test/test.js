6b304223eda068314615b595606516c929cb3bef
Kevin Schaul <kevin.schaul@gmail.com>
2013-10-17 15:48:53 -0500

    Fix line spacing in test.js

--- test/test.js
+++ test/test.js
@@ -138,54 +138,67 @@ describe('map', function() {
   });
 
   describe('formatPercent()', function() {
+
     it('Multiplies by 100 and rounds to two decimal places', function() {
       assert.equal(map.formatPercent(1), 100);
     });
+
     it('Multiplies by 100 and rounds to two decimal places', function() {
       assert.equal(map.formatPercent(0), 0);
     });
+
     it('Multiplies by 100 and rounds to two decimal places', function() {
       assert.equal(map.formatPercent(0.001), 0);
     });
+
     it('Multiplies by 100 and rounds to two decimal places', function() {
       assert.equal(map.formatPercent(0.005), 1);
     });
+
     it('Multiplies by 100 and rounds to two decimal places', function() {
       assert.equal(map.formatPercent(0.01), 1);
     });
+
     it('Multiplies by 100 and rounds to two decimal places', function() {
       assert.equal(map.formatPercent(0.9949), 99);
     });
+
     it('Multiplies by 100 and rounds to two decimal places', function() {
       assert.equal(map.formatPercent(0.995), 100);
     });
+
   });
 
   describe('formatAddress()', function() {
+
     it('Leaves addresses with `Minneapolis` in them alone', function() {
       assert.equal(
         map.formatAddress('111 Hennepin Ave, Minneapolis'),
         '111 Hennepin Ave, Minneapolis'
       );
     });
+
     it('Leaves addresses with `Minneapolis` in them alone', function() {
       assert.equal(
         map.formatAddress('111 Hennepin Ave, minneapolis'),
         '111 Hennepin Ave, minneapolis'
       );
     });
+
     it('Appends `Minneapolis, MN` to addresses without `Minneapolis` already in them', function() {
       assert.equal(
         map.formatAddress('111 Hennepin Ave'),
         '111 Hennepin Ave Minneapolis, MN'
       );
     });
+
     it('Leave zip codes alone', function() {
       assert.equal(
         map.formatAddress('55455'),
         '55455'
       );
     });
+
     it('Leave zip codes alone', function() {
       assert.equal(
         map.formatAddress('55455 Hennepin Ave'),
