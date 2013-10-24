e9cdee2cc4f598090f7c420ae559abf5ce05dba9
Kevin Schaul <kevin.schaul@gmail.com>
2013-10-24 17:41:41 -0500

    Added file watching to grunt commands

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
       }
     },
     php: {
-      dist: {
+      strib: {
         options: {
           hostname: '0.0.0.0',
           port: 8000,
@@ -29,16 +29,55 @@ module.exports = function(grunt) {
           open: true
         }
       }
+    },
+    copy: {
+      htmlToPhp: {
+        src: 'public/index.html',
+        dest: 'public/index.php'
+      },
+      phpToHtml: {
+        src: 'public/index.php',
+        dest: 'public/index.html'
+      }
+    },
+    watch: {
+      local: {
+        files: 'public/index.html',
+        tasks: 'copy:htmlToPhp'
+      },
+      strib: {
+        files: 'public/index.php',
+        tasks: 'copy:phpToHtml'
+      }
+    },
+    concurrent: {
+      local: {
+        tasks: ['watch:local', 'connect'],
+        options: {
+          logConcurrentOutput: true
+        }
+      },
+      strib: {
+        tasks: ['watch:strib', 'php'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-php');
   grunt.loadNpmTasks('grunt-mocha-test');
 
-  grunt.registerTask('default', ['connect']);
-  grunt.registerTask('strib', ['php']);
+  grunt.registerTask('local', ['concurrent:local']);
+  grunt.registerTask('strib', ['concurrent:strib']);
   grunt.registerTask('test', ['mochaTest']);
+  grunt.registerTask('default', ['local']);
 
 };
 
