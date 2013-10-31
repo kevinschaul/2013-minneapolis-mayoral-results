752cb81ca7dbd99892d87bfc3a55d380fdde2c08
stribchandra <chandra.akkari@startribune.com>
2013-10-31 14:08:21 -0500

    Changed verbiage in link that reloads citywide results.

--- public/index.php
+++ public/index.php
@@ -80,7 +80,7 @@
 <div class="wrapper precinct precinct-id-<%= precinctCandidates.id %>" data-id="<%= precinctCandidates.id %>">
   <div class="header">
     <span class="precinct-title">Precinct <%= precinctCandidates.id %></span>
-    <span class="return-total">Return to total results</span>
+    <span class="return-total">Return to citywide results</span>
     <span class="percents-headers">
       <span class="percent-header">1st choice</span>
       <span class="percent-header">2nd choice</span>
