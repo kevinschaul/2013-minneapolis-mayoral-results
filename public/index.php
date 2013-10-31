bfbc55b45d20e07a195e535ed9f35d61683e47aa
stribchandra <chandra.akkari@startribune.com>
2013-10-31 12:21:11 -0500

    Removed precinct reporting percentage from precint results list, modifications to index.php and base.css

--- public/index.php
+++ public/index.php
@@ -73,7 +73,6 @@
 <div class="wrapper precinct precinct-id-<%= precinctCandidates.id %>" data-id="<%= precinctCandidates.id %>">
   <div class="header">
     <span class="precinct-title">Precinct <%= precinctCandidates.id %></span>
-    <span class="reporting"><%= precinctsReporting %> reporting</span>
     <span class="return-total">Return to total results</span>
     <span class="percents-headers">
       <span class="percent-header">1st choice</span>
